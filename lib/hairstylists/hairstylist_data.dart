60fa5ead8edae1a367fdc103b536419c07214f6a
ChakaliMadhu6302769895 <Gauthamroyalz12324@gmail.com>
2024-01-03 10:26:18 +0530

    Initial commit

---
+++ lib/hairstylists/hairstylist_data.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const stylistData = [
+  {
+    'stylistName': 'John Dee',
+    'Description': 'Friends come and go, but a good hairstylist is forever.',
+    'rating': '4.8',
+    'rateAmount': '56',
+    'imgPth': 'assets/Scissors hair cut.jpg',
+    'bgColor': Color(0xffFFF0EB),
+  },
+  {
+    'stylistName': 'Mahesh Bhatt',
+    'Description': "Improve your selfies, don't cut your hair yourself!",
+    'rating': '4.7',
+    'rateAmount': '80',
+    'imgPth': 'assets/haircolor.jpeg',
+    'bgColor': Color(0xffEBF6FF),
+  },
+  {
+    'stylistName': 'Vinod Shetty',
+    'Description': 'Invest in your hair, it is the crown you never take off.',
+    'rating': '4.7',
+    'rateAmount': '70',
+    'imgPth': 'assets/shaving.jpeg',
+    'bgColor': Color(0xffFFF3EB),
+  }
+];
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:testing2/splash/splash.dart';
+
+import 'firebase_options.dart';
+
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
+  runApp( MyApp());
+}
+
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CustomSplashScreen(),
+    );
+  }
+}
